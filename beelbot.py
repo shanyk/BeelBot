fab600e841c4e911fe4a25c58199c8e9c91dfe47
shanyk <35853225+shanyk@users.noreply.github.com>
2018-11-02 16:51:21 -0400

    added update functionality
    added guilds to set command as well
    changed database to require input for all fields when first set
    
    after updating profile the bot will also calculate and display increases

--- beelbot.py
+++ beelbot.py
@@ -10,15 +10,15 @@ prefix = "$"
 # create discord bot
 bot = commands.Bot(command_prefix=prefix)
 
-# bot token
-token = 'NTA2MTMxOTE4OTc5NDY1MjI3.DrdtjQ.kbUpWr-G7nFSq3bWvfQ2nYWXbtA'
 
+token = None
 user = None
 pw = None
 
 with open('userpass.txt', 'r') as f:
 	user = f.readline().strip()
 	pw = f.readline().strip()
+	token = f.readline().strip()
 
 # bot is online
 @bot.event
@@ -31,14 +31,9 @@ async def ping(ctx):
 	latency = bot.latency
 	await ctx.send(latency)
 
-# bot echo test command
-@bot.command()
-async def echo(ctx, *, content:str):
-	await ctx.send(ctx.guild)
-	await ctx.send(ctx.author.roles)
-	await ctx.send(content)
-
-# role assignment command
+'''
+	gives the user the correct KL tag based on the KL they entered
+'''
 @bot.command()
 async def KL(ctx, arg):
 
@@ -71,10 +66,16 @@ async def KL(ctx, arg):
 	except (discord.Forbidden, discord.HTTPException) as e:
 		await ctx.send(f'{ctx.author.mention} {role_id} could not be assigned')
 
+'''
+	Catch all for all command errors. Sends the error string to the discord.
+'''
 @bot.event
 async def on_command_error(ctx, error):
 	await ctx.send(f'{ctx.author.mention} {error}')
 
+'''
+	command for users with the 'Admin' role on discord to turn off the discord bot
+'''
 @bot.command()
 async def offline(ctx):	
 	if 'Admin' in [role.name for role in ctx.author.roles]:	
@@ -84,14 +85,11 @@ async def offline(ctx):
 		await ctx.send('Nice try')
 		return
 
-# @bot.command()
-# async def updatemedals(ctx, arg):
-# 	pool = await asyncpg.create_pool(user=user, password=pw, database='beelbot')
-
-# 	missing = False
-
-# 	async with pool.acquire() as conn:
-
+'''
+	update command updates the database with new KL, medals, and mpm
+	will throw errors if data entered is less than what it was before
+	will also display stat increases
+'''
 @bot.command()
 async def update(ctx, KL, medals, mpm):
 	
@@ -113,39 +111,130 @@ async def update(ctx, KL, medals, mpm):
 	mpm_gain = None
 	medal_gain = None
 
+	guild = ''
+
 	async with pool.acquire() as con:
-		member = await con.fetchrow(f'SELECT kl, medals, mpm FROM profile WHERE id = {ID}')
+		member = await con.fetchrow(f'SELECT kl, medals, mpm, guild FROM profile WHERE id = {ID}')
 		old_KL = member['kl'] 
 		old_mpm = member['mpm']
 		old_medals = member['medals']
+		guild = member['guild']
 
 	medal_gain = calc_dif(old_medals, medals)
 	mpm_gain = calc_dif(old_mpm, mpm)
 	KL_gain = int(KL) - old_KL
 
-	await ctx.send((f'{ctx.author.mention} new KL {KL}\n'
-		f'```KLs gained: {KL_gain}\n'
-		f'medals gain: {medal_gain[0]}, {medal_gain[1]}\n'
-		f'mpm gain: {mpm_gain[0]}, {mpm_gain[1]}```'))
+	if medal_gain == None:
+		await ctx.send(('Please make sure you entered the correct amount of medals.\n'
+			f'You entered: {medals}\n'
+			f'Previous record was: {old_medals}\n'
+			f'No update will be made because medal gain is negative.'))
+		return
 
-	# if medal_gain[0] < 0:
-	# 	await ctx.send(('Please make sure you entered the correct amount of medals. '
-	# 		f'Your current gain is {medal_gain[0]} and %gain is {medal_gain[1]}'))
-	# 	return
+	br = '=' * 10
+	kl_prev = 'Previous KL: '
+	kl_new = 'New KL: '
+	kl_gain_str = 'KLs gained: '
+	md_prev = 'Previous Medals: '
+	md_new = 'New Medals: '
+	md_gain_str = 'Medals gained: '
+	md_percent = 'Medal gain %: '
+	mpm_prev = 'Previous MPM: '
+	mpm_new = 'New MPM: '
+	mpm_gain_str = 'MPM gain: '
+	mpm_percent = 'MPM gain %: '
 
+	update_str = (f'{ctx.author.mention}\n'
+		f'```{ctx.author.display_name}   KL {KL}\n'
+		f'{guild}\n'
+		f'{br}\n'
+		f'{kl_prev:<13} {old_KL:>4}\n'
+		f'{kl_new:<13} {KL:>4}\n'
+		f'{kl_gain_str:<13} {KL_gain:>4}\n'
+		f'{br}\n'
+		f'{md_prev:<17} {old_medals:>6}\n'
+		f'{md_new:<17} {medals:>6}\n'
+		f'{md_gain_str:<17} {medal_gain[0]:>6}\n'
+		f'{md_percent:<17} {medal_gain[1]:>6}\n'
+		f'{br}\n'
+		f'{mpm_prev:<14} {old_mpm:>6}\n'
+		f'{mpm_new:<14} {mpm:>6}\n'
+		f'{mpm_gain_str:<14} {mpm_gain[0]:>6}\n'
+		f'{mpm_percent:<14} {mpm_gain[1]:>6}\n```')
+
+	await ctx.send(update_str)
+
+	async with pool.acquire() as con:
+		await con.execute((f'UPDATE profile SET mpm = \'{mpm}\', medals = \'{medals}\', kl = {int(KL)}'
+			f'WHERE id = {ctx.author.id}'))
+		await ctx.send('Profile updated successfully!')
+
+	await pool.close()
+
+	
+'''
+	first command that any new member of the discord should call in order to set their profile
+	for the first time
+'''
 @bot.command()
-async def set(ctx, KL, medals, mpm):
+async def set(ctx, KL, medals, mpm, guild):
 
 	pool = await asyncpg.create_pool(user=user, password=pw, database='beelbot')
 
 	async with pool.acquire() as con:
-		await ctx.send('connection established')
-		await con.execute((f'INSERT INTO profile (id, name, medals, mpm, kl) '
-			f'VALUES ({ID}, \'{name}\', \'{medals}\', \'{mpm}\', \'KL\')'))
+		await con.execute((f'INSERT INTO profile (id, name, medals, mpm, kl, guild) '
+			f'VALUES ({ctx.author.id}, \'{ctx.author.display_name}\', \'{medals}\', \'{mpm}\', \'{KL}\', \'{guild}\')'))
+		await ctx.send(f'{ctx.author.mention} you have been entered into the database!')
 
 	await pool.close()
 
+'''
+	profile command lets user see their personal stats
+	name KL
+	guild
+	total medals
+	mpm
+'''
+@bot.command()
+async def profile(ctx):
 
+	pool = await asyncpg.create_pool(user=user, password=pw, database='beelbot')
+
+	KL = ''
+	medals = ''
+	mpm = ''
+	guild = ''
+
+	async with pool.acquire() as con:
+		member = await con.fetchrow((f'SELECT kl, medals, mpm, guild FROM profile WHERE id = {ctx.author.id}'))
+		KL = member['kl']
+		medals = member['medals']
+		mpm = member['mpm']
+		guild = member['guild']
+
+	total = 'Total Medals: '
+	current = 'Current MPM: '
+	br = '=' * 10
+
+	profile_str = (f'{ctx.author.mention}\n'
+		f'```{ctx.author.display_name}   KL {KL}\n'
+		f'{guild}\n'
+		f'{br}\n'
+		f'{total:<14} {medals:>5}\n'
+		f'{current:<14} {mpm:>5}```'
+		)
+
+	await ctx.send(profile_str)
+
+	await pool.close()
+
+'''
+	calc_dif is used to calculate the difference between old medals and new medals 
+	as well as old mpm and new mpm. medals and mpm are passed into the function as string.
+
+	two strings are returned as a number+char representation and a percentage representation
+	returns None if the gain is negative because this is indicative of data entry error
+'''
 def calc_dif(old, new):
 
 	old_num = float(old[:-1])
@@ -165,7 +254,7 @@ def calc_dif(old, new):
 
 		return (gain_str, gain_percent_str)
 	elif gain >= 0:
-		gain_str = f'{gain:.2f}{old_char}'
+		gain_str = f'{gain:.1f}{old_char}'
 		gain_percent_str = f'{gain_percent:.2f}%'
 
 		return (gain_str, gain_percent_str)
