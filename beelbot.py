d93e77af79cf26401b0e733459624824cfc3370f
shanyk <35853225+shanyk@users.noreply.github.com>
2018-11-02 02:53:35 -0400

    bot can also pull data from the db

--- beelbot.py
+++ beelbot.py
@@ -93,7 +93,7 @@ async def offline(ctx):
 # 	async with pool.acquire() as conn:
 
 @bot.command()
-async def set(ctx, medals, mpm):
+async def update(ctx, KL, medals, mpm):
 	
 	# await ctx.send(f'{type(ctx.author.id)} {ctx.author.id}')
 	# await ctx.send(f'{type(ctx.author.display_name)} {ctx.author.display_name}')
@@ -104,20 +104,73 @@ async def set(ctx, medals, mpm):
 
 	ID = ctx.author.id
 	name = ctx.author.display_name
-	
+
+	old_KL = None
+	old_mpm = None
+	old_medals = None
+
+	KL_gain = None
+	mpm_gain = None
+	medal_gain = None
+
+	async with pool.acquire() as con:
+		member = await con.fetchrow(f'SELECT kl, medals, mpm FROM profile WHERE id = {ID}')
+		old_KL = member['kl'] 
+		old_mpm = member['mpm']
+		old_medals = member['medals']
+
+	medal_gain = calc_dif(old_medals, medals)
+	mpm_gain = calc_dif(old_mpm, mpm)
+	KL_gain = int(KL) - old_KL
+
+	await ctx.send((f'{ctx.author.mention} new KL {KL}\n'
+		f'```KLs gained: {KL_gain}\n'
+		f'medals gain: {medal_gain[0]}, {medal_gain[1]}\n'
+		f'mpm gain: {mpm_gain[0]}, {mpm_gain[1]}```'))
+
+	# if medal_gain[0] < 0:
+	# 	await ctx.send(('Please make sure you entered the correct amount of medals. '
+	# 		f'Your current gain is {medal_gain[0]} and %gain is {medal_gain[1]}'))
+	# 	return
+
+@bot.command()
+async def set(ctx, KL, medals, mpm):
+
+	pool = await asyncpg.create_pool(user=user, password=pw, database='beelbot')
+
 	async with pool.acquire() as con:
 		await ctx.send('connection established')
-		await con.execute((f'INSERT INTO profile (id, name, medals, mpm) '
-			f'VALUES ({ID}, \'{name}\', \'{medals}\', \'{mpm}\')'))
+		await con.execute((f'INSERT INTO profile (id, name, medals, mpm, kl) '
+			f'VALUES ({ID}, \'{name}\', \'{medals}\', \'{mpm}\', \'KL\')'))
 
 	await pool.close()
 
-# @bot.command()
-# async def reset(ctx):
-# 	await ctx.send('Reseting')
-# 	await bot.close()
-# 	run()
-# 	await ctx.send('Reset complete')
 
-# client.run(token)
+def calc_dif(old, new):
+
+	old_num = float(old[:-1])
+	old_char = old[-1:]
+
+	new_num = float(new[:-1])
+	new_char = new[-1:]
+
+	multiplier = (ord(new_char) - ord(old_char)) * 1000
+
+	gain = new_num - old_num if multiplier == 0 else new_num * multiplier - old_num
+	gain_percent = (gain/old_num) * 100
+
+	if gain >= 1000:
+		gain_str = f'{gain/1000:.2f}{new_char}'
+		gain_percent_str = f'{gain_percent:.2f}%'
+
+		return (gain_str, gain_percent_str)
+	elif gain >= 0:
+		gain_str = f'{gain:.2f}{old_char}'
+		gain_percent_str = f'{gain_percent:.2f}%'
+
+		return (gain_str, gain_percent_str)
+	else: 
+		return None
+
 bot.run(token)
+
